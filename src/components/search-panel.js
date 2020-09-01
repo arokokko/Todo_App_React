b9d2b04288e29bad1ebcd3569739188a0a46794e
arokokko <56476810+arokokko@users.noreply.github.com>
2020-09-01 15:34:17 +0700

    Realize module structure of project
    Divide project to separate modules, import-export modules to index.js

---
+++ src/components/search-panel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchPanel = () => {
+    return <input type='text' placeholder='search'></input>;
+};
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-  const cake = <li>Eat pancake</li>;
-  return (
-    <ul>
-      <li>{ (new Date()).toString() }</li>
-      { cake }
-      <li>Drink coffee</li>
-      <li>Learn React</li>
-      <li>Create React App</li>
-    </ul>
-  );
-};
-
-const AppHeader = () => {
-  return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-  return <input type='text' placeholder='search'></input>;
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
   return (
