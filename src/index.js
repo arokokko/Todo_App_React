f0a8fea1a9ae08584058bb42bf3629b73bd985f9
arokokko <56476810+arokokko@users.noreply.github.com>
2020-09-01 15:11:41 +0700

    Create first React components, render it to index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+  const cake = <li>Eat pancake</li>;
+  return (
+    <ul>
+      <li>{ (new Date()).toString() }</li>
+      { cake }
+      <li>Drink coffee</li>
+      <li>Learn React</li>
+      <li>Create React App</li>
+    </ul>
+  );
+};
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input type='text' placeholder='search'></input>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.querySelector('#root'));
