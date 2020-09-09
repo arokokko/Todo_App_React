5e47f3979666ec1150d9be58c8a105ea56af3c12
arokokko <56476810+arokokko@users.noreply.github.com>
2020-09-09 14:07:02 +0700

    Add styles for all components

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './search-panel.css';
 
 const SearchPanel = () => {
-    return <input type='text' placeholder='search'></input>;
+    return <input type='text' className="form-control search-input" placeholder='type to search'></input>;
 };
 
 export default SearchPanel;
