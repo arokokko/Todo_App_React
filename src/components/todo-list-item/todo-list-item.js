91a9919f5de28c7c45368888370c5d88b4a6033e
arokokko <56476810+arokokko@users.noreply.github.com>
2020-11-20 21:04:43 +0700

    Realize function mark items as important and done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,27 +3,60 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+	
+	constructor() {
+		super();
+
+		this.state = {
+			done: false,
+			important: false
+		}
+
+		this.onLabelClick = () => {
+			this.setState((state) => {
+				return {
+					done: !state.done
+				}
+			});
+		}
+
+		this.onMarkImportant = () => {
+			this.setState((state) => {
+				return {
+					important: !state.important
+				}
+			});
+		}
+
+	}
+
 	render() {
 
-		const { label, important = false } = this.props;
-		const style = {
-			color: important ? 'steelblue' : 'black',
-			fontWeight: important ? 'bold' : 'normal'
-		};
-	
+		const { label } = this.props;
+		const { done, important } = this.state;
+		
+
+		let classNames = 'todo-list-item';
+		if (done) {
+			classNames += ' done';
+		}
+
+		if(important) {
+			classNames += ' important';
+		}
+
 		return (
-			<span className="todo-list-item">
-				<span className="todo-list-item-label" style={style}>
+			<span className={classNames}>
+				<span className="todo-list-item-label" onClick={ this.onLabelClick }>
 					{label}
 				</span>
 	
 				<button type="button"
-					className="btn btn-outline-success btn-sm float-right">
+					className="btn btn-outline-success btn-sm float-right"  onClick={ this.onMarkImportant }>
 					<i className="fa fa-exclamation" />
 				</button>
 	
-				<button type="button"
-					className="btn btn-outline-danger btn-sm float-right">
+				<button type="button" className="btn btn-outline-danger btn-sm float-right">
 					<i className="fa fa-trash-o" />
 				</button>
 			</span>
